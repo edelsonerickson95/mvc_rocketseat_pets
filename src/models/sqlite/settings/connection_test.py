10adabf6ac70694474d86686d8fef04ffcf25bad
Edelson Erikson <edelsonerickson>
2026-08-15 08:42:00 -0300

    config: implement models dir with connection to sqlite

---
+++ src/models/sqlite/settings/connection_test.py
@@ -0,0 +1,14 @@
+import pytest
+from connection import db_connection_handler
+from sqlalchemy.engine import Engine
+
+
+@pytest.mark.skip(reason="interacao com banco")
+def test_connection_to_db():
+    assert db_connection_handler.get_engine() is None
+
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
