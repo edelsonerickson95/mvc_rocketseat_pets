0a20d5a89b4e7e42b4af786bccad3f08b473e286
Edelson Erikson <edelsonerickson@hotmail.com>
2026-08-17 10:36:29 -0300

    feat: Implementing person_creator_controller with tests

---
+++ src/controllers/person_creator_controller_test.py
@@ -0,0 +1,37 @@
+import pytest
+from .person_creator_controller import PersonCreatorController
+
+
+class MockPeopleRepository:
+    def insert_person(self, first_name: str, last_name: str, age: int, pet_id: int):
+        pass
+
+
+def test_create():
+    person_infor = {
+        "first_name": "Edelson",
+        "last_name": "Erikson",
+        "age": 31,
+        "pet_id": 8,
+    }
+
+    controller = PersonCreatorController(MockPeopleRepository())
+    response = controller.create(person_infor)
+
+    assert response["data"]["type"] == "Person"
+    assert response["data"]["count"] == 1
+    assert response["data"]["attributes"] == person_infor
+
+
+def test_create_error():
+    person_infor = {
+        "first_name": "Edelson12",
+        "last_name": "Erikson",
+        "age": 31,
+        "pet_id": 8,
+    }
+
+    controller = PersonCreatorController(MockPeopleRepository())
+    with pytest.raises(Exception):
+        controller.create(person_infor)
+
--- src/models/controllers/person_creator_controller.py
+++
@@ -1,6 +0,0 @@
-# from src.models.sqlite.interfaces.people_repository import PeopeRepositoryInterface
-
-
-# class PersonCreatorController:
-#     def __init__(self, peope_repository: PeopeRepositoryInterface) -> None:
-#         self.__people_repository = peope_repository
